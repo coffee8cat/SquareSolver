9c1943b8d2d291140df8ea3079bdf88aee174050
coffee8cat <ierandil@yandex.ru>
2024-08-26 12:53:26 +0300

    added my_macros.h documentation

--- headers/my_macros.h
+++ headers/my_macros.h
@@ -1,3 +1,8 @@
+/**
+ * \file my_macros.h
+ * \brief my own version of some macros: assert, isnan, isinf
+ */
+
 #ifndef MY_SQSOLVER_MACROS_H__
 #define MY_SQSOLVER_MACROS_H__
 
@@ -11,6 +16,11 @@
 
 #define my_assert(expr) my_assert_f(expr, #expr)
 
+/**
+ * \brief my version of assert
+ * \param expr - expression to check
+ * \param expression - text representation of original expression
+ */
 inline void my_assert_f(int expr, const char expression[])
 {
     if (expr == 0)
